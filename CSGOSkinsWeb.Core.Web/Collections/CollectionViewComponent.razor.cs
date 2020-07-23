657e7002052d9a6e2b2af0839daee6720c5dde0e
Mikkel Buhl <52383121+Zorxh@users.noreply.github.com>
2020-07-24 01:09:36 +0200

    Added Collection View Component.

---
+++ CSGOSkinsWeb.Core.Web/Collections/CollectionViewComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using CSGOSkinsWeb.Core.Services;
+using CSGOSkinsWeb.Database.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CSGOSkinsWeb.Core.Web.Collections
+{
+    public partial class CollectionViewComponent : OwningComponentBase
+    {
+        [Parameter]
+        public WeaponCollection WeaponCollection { get; set; }
+
+    }
+}
---
+++ CSGOSkinsWeb.Web/Pages/CollectionPages/CollectionViewPage.razor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using CSGOSkinsWeb.Database.Models;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
+using System.Threading.Tasks;
+using CSGOSkinsWeb.Core.Services;
+using CSGOSkinsWeb.Core.Web.Skins;
+
+namespace CSGOSkinsWeb.Web.Pages.CollectionPages
+{
+    public partial class CollectionViewPage : ComponentBase
+    {
+        [Inject]
+        public ICollectionService CollectionService { get; set; }
+        [Inject]
+        public ISkinService SkinService { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string Idstring { get; set; }
+
+        private WeaponCollection weaponCollection;
+        private List<Skin> weaponSkins;
+        private List<Skin> weaponSkinsByCollectionId = new List<Skin>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            weaponCollection = await CollectionService.GetCollectionByIdStringAsync(Idstring).ConfigureAwait(false);
+            weaponSkins = await SkinService.GetSkinsAsync().ConfigureAwait(false);
+
+            SortSkinList();
+
+            if (weaponCollection == null)
+            {
+                NavigationManager.NavigateTo("/collections");
+                return;
+            }
+        }
+
+        private void SortSkinList()
+        {
+            foreach (var skin in weaponSkins.Where(skin => skin.Weapcoll == weaponCollection.Id))
+            {
+                weaponSkinsByCollectionId.Add(skin);
+            }
+        }
+
+        private void GoToEditPage()
+        {
+            NavigationManager.NavigateTo($"/collections/edit/{weaponCollection.Idstring}");
+        }
+    }
+}
